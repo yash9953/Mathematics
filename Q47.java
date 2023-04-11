f88cddcd04d47303b613f2fa8d7ba7797d4afcda
Yash Tyagi <yashtyagi9953@gmail.com>
2023-04-11 06:21:47 +0530

    Add files via upload

---
+++ Q47.java
@@ -0,0 +1,9 @@
+public class Q47 {
+          public static void main(String[] args) {
+                    int i=70;
+                    for(;i>60;){        
+                              System.out.print(" "+i);
+                              i--;
+                    }
+          }
+}
