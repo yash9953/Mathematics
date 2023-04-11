4dd0fec96227561ba9d91088d15911015f329872
Yash Tyagi <yashtyagi9953@gmail.com>
2023-04-11 06:16:10 +0530

    Add files via upload

---
+++ RemoveDuplicate.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RemoveDuplicate {
+          // static ArrayList<Integer> removeduplicate(int a[]){
+                    // int temp=0,c=0;
+                    // ArrayList<Integer> list=new ArrayList<>();
+                    // for(int i=0;i<a.length;i++){
+                    //           temp=a[i];
+                    //           for(int j=i+1;j<a.length;j++){
+                    //                     if(temp==a[j]){
+                    //                               c++;
+                    //                     }
+                    //                     if(temp<a[j]){
+                    //                               break;
+                    //                     }
+                    //           }
+                    //           list.add(temp);
+                    // }
+                    // for(int i=0;i<c;i++){
+                    //           list.add(-1);
+                    // }
+                    // return list;
+          // }
+          public static void main(String[] args) {
+                    Scanner sc=new Scanner(System.in);
+                    // int a[]={1,1,2,2,2,3,3};
+                    // System.out.println(removeduplicate(a));
+                    // ArrayList<Integer> m=removeduplicate(a);
+                    // System.out.println(m);
+                    sc.close();
+          }
+}
