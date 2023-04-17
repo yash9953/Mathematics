6af0d7e6e102eec3a6ddd761a1dca16af6a19b0f
Yash Tyagi <yashtyagi9953@gmail.com>
2023-04-17 20:13:39 +0530

    Add files via upload

---
+++ RearrangeArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RearrangeArray {
+          // codestudio question 
+          public static void main(String[] args) {
+                    Scanner sc=new Scanner(System.in);
+                    int n=sc.nextInt();
+                    int arr[]=new int[n];
+                    for(int i=0;i<n;i++){
+                              arr[i]=sc.nextInt();
+                    }
+                    for(int i=0;i<n;i++){
+                              System.out.print(arr[arr[i]]+" ");
+                    }
+                    sc.close();
+          }
+}
