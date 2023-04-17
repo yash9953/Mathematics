75de705f1d4cf363f3f7d7fef4e777c1af5ad631
Yash Tyagi <yashtyagi9953@gmail.com>
2023-04-17 20:14:14 +0530

    Add files via upload

---
+++ Q.java
@@ -0,0 +1,14 @@
+public class Q {
+          public static void main(String[] args) {
+                    int a=5;
+                    int b=5;
+                    if(a>>2<++b){
+                              // a=a>>2;
+                              System.out.println(a);
+                              System.out.println(b);
+                              System.out.println("a is less than b");
+                    }else{
+                              System.out.println("a is greater than b");
+                    }
+          }
+}
