8a749ff59c35190596912e48e47c8d0057313687
Yash Tyagi <yashtyagi9953@gmail.com>
2023-04-17 20:11:52 +0530

    Add files via upload

---
+++ GCDGFG.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class GCDGFG {
+          public static void main(String[] args) {
+                    Scanner sc=new Scanner(System.in);
+                    int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+                    for(int i=0;i<a.length;i++){
+                              // for(int j=i;j<a[i].length;i++){
+                                        System.out.println(a[i].length);
+
+                              // }
+                    }
+                    sc.close();
+          }
+}
